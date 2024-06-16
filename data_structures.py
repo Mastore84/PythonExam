0fa2ac44f5f0e4ff1cfa745492f1ba21237778b1
Mastore84 <76755222+Mastore84@users.noreply.github.com>
2024-06-16 19:00:06 +0000

    Updated data_structures for exam

--- data_structures.py
+++ data_structures.py
@@ -1,18 +1,25 @@
 # Lists
-my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5, 3]
 print(my_list)
+my_list.append(6)
+print(my_list)
+my_list.remove(3)
+print(my_list)
+print("")
 
 # Tuples
-my_tuple = (2, 1, 3, 4, 5)
+my_tuple = (2, 1, 3, 2, 4, 5)
 print(my_tuple)
-
-# Dictionaries
-my_dict = {'apple': 3, 'banana': 2, 'orange': 5}
-print(my_dict)
+print("")
 
 # Sets
 my_set = {4, 5, 1, 3, 2}
 print(my_set)
+print("")
+
+# Dictionaries
+my_dict = {'banana': 2, 'orange': 5, 'apple': 3}
+print(my_dict)
 print("")
 
 # Iterating over a list
